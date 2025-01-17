33517c9aac55823ae3be491fc6f548c368477073
santiago gamez <gamez.guerra18@gmail.com>
2025-01-16 22:09:30 -0500

    fix ultralence

--- public/js/ultralence.js
+++ public/js/ultralence.js
@@ -1,1 +1,44 @@
-function slide(e,i){document.getElementById("slide-"+e)?slideNum=e:slideNum=1,document.getElementById("slide-"+i).className="slide",document.getElementById("slide-"+slideNum).className="slide-active";var s=slideNum+1;timeSlide=setTimeout(function(){slide(s,slideNum)},1e4),stopAutoSlide()}function stopAutoSlide(){clearTimeout(timeSlide)}setTimeout(function(){slide(2,1)},1e4);var timeSlide;$(document).ready(function(){$(".sidebar-menu > li.nav-dropdown a").on("click",function(e){$(this).parent().hasClass("active")?($(this).next().slideToggle(),$(".sidebar-menu li").removeClass("active")):($(".sidebar-menu li ul").slideUp(),$(this).next().slideToggle(),$(".sidebar-menu li").removeClass("active"),$(this).parent().addClass("active"))})});
+function slide(e,i) {
+    var nextSlide = document.getElementById("slide-"+e);
+    var currentSlide = document.getElementById("slide-"+i);
+    
+    if (!nextSlide || !currentSlide) {
+        return;
+    }
+    
+    slideNum = nextSlide ? e : 1;
+    currentSlide.className = "slide";
+    document.getElementById("slide-"+slideNum).className = "slide-active";
+    
+    var s = slideNum + 1;
+    timeSlide = setTimeout(function(){
+        slide(s,slideNum);
+    }, 10000);
+    stopAutoSlide();
+}
+
+function stopAutoSlide() {
+    clearTimeout(timeSlide);
+}
+
+if (document.getElementById("slide-1")) {
+    setTimeout(function(){
+        slide(2,1);
+    }, 10000);
+}
+
+var timeSlide;
+
+$(document).ready(function(){
+    $(".sidebar-menu > li.nav-dropdown a").on("click",function(e){
+        if($(this).parent().hasClass("active")){
+            $(this).next().slideToggle();
+            $(".sidebar-menu li").removeClass("active");
+        }else{
+            $(".sidebar-menu li ul").slideUp();
+            $(this).next().slideToggle();
+            $(".sidebar-menu li").removeClass("active");
+            $(this).parent().addClass("active");
+        }
+    });
+});
